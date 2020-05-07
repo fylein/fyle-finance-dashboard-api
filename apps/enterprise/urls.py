0e29e3b7d17ffc13e550d9b7401932f8bb6c999a
sheshantsinha <b216040@iiit-bh.ac.in>
2020-05-07 16:38:02 +0530

    Changed name

--- apps/enterprise/urls.py
+++ apps/enterprise/urls.py
@@ -1,4 +1,4 @@
-"""fyle_qbo URL Configuration
+"""fyle_financial_dashboard_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
