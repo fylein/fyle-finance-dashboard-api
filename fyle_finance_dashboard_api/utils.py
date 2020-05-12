40b0412f64fb308ba3f73536146772e9867d01a3
sheshantsinha <b216040@iiit-bh.ac.in>
2020-05-12 14:22:22 +0530

    comment resolved

--- fyle_finance_dashboard_api/utils.py
+++ fyle_finance_dashboard_api/utils.py
@@ -1,7 +1,6 @@
 from rest_framework.views import Response
 from rest_framework.serializers import ValidationError
 
-
 def assert_valid(condition: bool, message: str) -> Response or None:
     """
     Assert conditions
@@ -14,64 +13,36 @@ def assert_valid(condition: bool, message: str) -> Response or None:
             'message': message
         })
 
-def fund_source(value):
-    if value == "PERSONAL":
-        value = "Personal Account"
-    elif value == "ADVANCE":
-        value = "Advance"
-    return value
 
-
-def cast_date(value):
+def get_date(value):
     if value:
         value = value.split("T")[0].split("-")[::-1]
         return "-".join(value)
     return value
 
 
-def state(value):
-    if value == "PAYMENT_PROCESSING":
-        value = "Payment Processing"
-    elif value == "COMPLETE":
-        value = "Complete"
-    elif value == "PAYMENT_PENDING":
-        value = "Payment Pending"
-    elif value == "APPROVED":
-        value = "Approved"
-    elif value == "APPROVER_PENDING":
-        value = "Approver pending"
-    return value
+FUND_SOURCE = {
+    "PERSONAL": "Personal Account",
+    "ADVANCE": "Advance",
+    "CCC": "Corporate Credit Card"
+}
 
+STATES = {
+    "PAYMENT_PROCESSING": "Payment Processing",
+    "COMPLETE": "Complete",
+    "PAYMENT_PENDING": "Payment Pending",
+    "APPROVED": "Approved",
+    "APPROVER_PENDING": "Approver pending",
+    "DRAFT": "Draft",
+    "PAID": "Paid"
+}
 
-def reimbursable(value):
-    if value:
-        value = "YES"
-    else:
-        value = "NO"
-    return value
+REIMBURSABLE = {
+    1: "YES",
+    0: "NO"
+}
 
-keys = [
-    "employee_email",
-    "employee_id",
-    "spent_at",
-    "currency",
-    "amount",
-    "purpose",
-    "cost_center_name",
-    "category_name",
-    "sub_category",
-    "expense_number",
-    "report_id",
-    "state",
-    "fund_source",
-    "reimbursable",
-    "created_at",
-    "approved_at",
-    "org_name",
-    "claim_number"
-]
-
-map = {
+EXPENSE_COLUMN_NAMES = {
     "employee_email": "Employee Email",
     "employee_id": "Employee Code",
     "spent_at": "Spent on",
@@ -92,18 +63,21 @@ map = {
     "claim_number": "Claim Number"
 }
 
-value_casting = {
-    "spent_at": cast_date,
-    "created_at": cast_date,
-    "approved_at": cast_date,
-    "fund_source": fund_source,
-    "reimbursable": reimbursable,
-    "state": state
+get_value_from_dict = {
+    "fund_source": FUND_SOURCE,
+    "reimbursable": REIMBURSABLE,
+    "state": STATES
+}
+
+get_value_from_function = {
+    "spent_at": get_date,
+    "created_at": get_date,
+    "approved_at": get_date
 }
 
 
-def headers():
-    return [map[head] for head in keys]
+def get_headers():
+    return [head for head in EXPENSE_COLUMN_NAMES]
 
 
 def formatting_specific_key(key, value):
@@ -112,5 +86,17 @@ def formatting_specific_key(key, value):
     return value
 
 
-def format_tpa(tpas):
-    return [[formatting_specific_key(key, tpa[key]) for key in keys] for tpa in tpas]
+def format_expenses(expenses):
+    data_to_export = []
+    for expense in expenses:
+        single_expense = []
+        for column_name in EXPENSE_COLUMN_NAMES:
+            column_value = expense[column_name]
+            if column_name in get_value_from_dict:
+                column_value = get_value_from_dict[column_name].get(expense[column_name], expense[column_name])
+            if column_name in get_value_from_function:
+                column_value = get_date(expense[column_name])
+            single_expense.append(column_value)
+        data_to_export.append(single_expense)
+
+    return data_to_export
