a636bd05d5be3db0c69a03d3f244c07689832e41
sheshantsinha <b216040@iiit-bh.ac.in>
2020-05-12 14:55:44 +0530

    Added sharer

--- apps/exports/gsheet.py
+++ apps/exports/gsheet.py
@@ -2,7 +2,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient import discovery
 import gspread
 from ..fyle.utils import FyleConnector
-from fyle_finance_dashboard_api.utils import format_tpa, headers
+from fyle_finance_dashboard_api.utils import format_expenses, get_headers
 scope = [
             'https://www.googleapis.com/auth/analytics.readonly',
             'https://www.googleapis.com/auth/drive',
@@ -16,17 +16,26 @@ class GoogleSpreadSheet:
         self.client = gspread.authorize(creds)
         self.service = discovery.build('sheets', 'v4', credentials=creds)
         self.range_ = 'A1:Z'
+        self.SYNC_SUCCESSFUL = "Sync Completed"
+        self.SYNC_FAILED = "Sync Failed"
+        self.DEFAULT_SYNC_STATUS = "Sync Pending"
 
-    def create_sheet(self, share_with):
+    def create_sheet(self):
         sheet = self.client.create('Fyle-GDS')
-        sheet.share(share_with, perm_type='user', role='writer')
         return sheet.id
 
+    def share_sheet(self, sheet_id, email_id):
+        sheet = self.client.open_by_key(sheet_id)
+        sheet.share(email_id, perm_type='user', role='writer')
+
     def write_data(self, orgs, sheet_id):
-        data_to_export = [headers()]
+        data_to_export, total_orgs = [get_headers()], 0
         for org in orgs.values():
-            fyle_tpa_data = FyleConnector(org['refresh_token'])
-            data_to_export += format_tpa(fyle_tpa_data.get_fyle_tpa())
+            if org['refresh_token'] is not None:
+                fyle_tpa_data = FyleConnector(org['refresh_token'])
+                data_to_export += format_expenses(fyle_tpa_data.get_fyle_tpa())
+                total_orgs += 1
+
         value_range_body = {
             'values': data_to_export
         }
@@ -40,5 +49,5 @@ class GoogleSpreadSheet:
         response = request.execute()
         rows = len(data_to_export)-1
         if response:
-            return True, rows
-        return False, 0
+            return True, rows, total_orgs
+        return False, 0, total_orgs, SYNC_FAILED
